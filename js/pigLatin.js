f439ec7c2a4ade4179700de91b501d5dc458fb1d
afelix997 <afelix2013@gmail.com>
2022-06-06 15:19:15 -0700

    answer

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,11 @@
-exports.translate = function(word) {
+const translate = function (inStr) {
+    let vowel = 'aeiou'.split('')
+    let cons= 'bcdfghjklmnpqrstvwxyz'.split('')
+    let inArr =  inStr.split(/([_\W])/)
+    
+
+    
+
+    return inArr
 };
+console.log(translate("Hi, I'm Zach.\nHow are you?"))
--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,26 @@
+import re
 def translate(word_or_phrase):
-  pass
+  vowel = list('aeiouAEIOU')
+  cons= list('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ')
+  in_list =  re.findall(r"[\w']+|[.,!?;\n]",word_or_phrase )
+
+  for i,word in enumerate(in_list):
+    if word[0].isalpha():
+      while in_list[i][0] not in vowel:
+        in_list[i]=in_list[i][1:] + in_list[i][0]       
+      if in_list[i][0] in vowel:
+        in_list[i]= in_list[i] + 'ay'
+    for letter in word:
+      if letter.isupper():
+        fix=list(in_list[i].lower())
+        fix[0]=fix[0].upper()
+        fix = ''.join(fix)
+        in_list[i]=fix
+                                        
+  result=' '.join(in_list)
+  return result
+       
+
+
+  
 
